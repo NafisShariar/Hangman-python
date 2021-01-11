0f956fc51ad5624716e427673935fdb135094d3d
NafisShariar <nafisshariar34@gmail.com>
2021-01-12 02:52:02 +0600

    fixed values

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# Step 5
-
 from hangman_art import logo
 import random
 
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-# Delete this line: word_list = ["ardvark", "baboon", "camel"]
+# Update the word list to use the 'word_list' from hangman_words.py
+
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -13,7 +11,7 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+# Import the logo from hangman_art.py and print it at the start of the game.
 print(logo)
 
 # Testing code
@@ -27,7 +25,7 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    #  If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
 
@@ -40,7 +38,7 @@ while not end_of_game:
 
     # Check if user is wrong.
     if guess not in chosen_word:
-        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
 
         lives -= 1
@@ -56,6 +54,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    # TODO-2: - Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
